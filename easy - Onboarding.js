5e109b58070e3dfbd1d2ca45caf1fce6a0e34638
e1r0nd <a.merezhanyi@gmail.com>
2017-01-15 14:52:55 +0100

    small fix

--- easy - Onboarding.js
+++ easy - Onboarding.js
@@ -5,8 +5,7 @@
  * (standard input parsing, game loop, writing to the standard output).
  * You can discover the platform with this simple exercise of basic comparison
  * of values and try up to 25 programming languages.
- * External resources ConditionsStandard streams
- * Statement
+ * Statement:
  * A tutorial mission for all newcomers: your program must find which of the
  * two targets is the closest.
 **/

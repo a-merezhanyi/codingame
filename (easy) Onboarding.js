13a8731a6eaf9268d62434b29ffcb9446659085f
e1r0nd <a.merezhanyi@gmail.com>
2017-01-15 14:57:02 +0100

    update doc

--- (easy) Onboarding.js
+++ (easy) Onboarding.js
@@ -1,10 +1,9 @@
 /** Onboarding (Easy) https://www.codingame.com/training/easy/onboarding
- * What will I learn?
- * Conditions
  * Solving this puzzle helps you understand how the puzzles work on CodinGame
  * (standard input parsing, game loop, writing to the standard output).
  * You can discover the platform with this simple exercise of basic comparison
  * of values and try up to 25 programming languages.
+ *
  * Statement:
  * A tutorial mission for all newcomers: your program must find which of the
  * two targets is the closest.

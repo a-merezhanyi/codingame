07dcb47c0c08ab9b469afe18306939df2deb7a0c
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2017-08-23 00:08:23 +0200

    Onboarding - solved

--- (easy) Onboarding.js
+++ (easy) Onboarding.js
@@ -1,27 +1,18 @@
 /** Onboarding (Easy) https://www.codingame.com/training/easy/onboarding
  * Solving this puzzle helps you understand how the puzzles work on CodinGame
  * (standard input parsing, game loop, writing to the standard output).
- * You can discover the platform with this simple exercise of basic comparison
- * of values and try up to 25 programming languages.
+ * You can discover the platform with this simple exercise of basic 
+ * comparison of values and try up to 25 programming languages.
  *
  * Statement:
- * A tutorial mission for all newcomers: your program must find which of the
- * two targets is the closest.
+ * our program must destroy the enemy ships by shooting the closest enemy
+ * on each turn.
 **/
 while (true) {
-    var count = parseInt(readline()); // The number of current enemy ships within range
-    var closestEnemy = null,
-        minDistance = 1000;
-    for (var i = 0; i < count; i++) {
-        var inputs = readline().split(' ');
-        var enemy = inputs[0]; // The name of this enemy
-        var dist = parseInt(inputs[1]); // The distance to your cannon of this enemy
-        if (dist < minDistance){
-            minDistance = dist;
-            closestEnemy = enemy;
-            printErr(closestEnemy, dist, minDistance);
-        }
-    }
+    const enemy1 = readline(); // name of enemy 1
+    const dist1 = +readline(); // distance to enemy 1
+    const enemy2 = readline(); // name of enemy 2
+    const dist2 = +readline(); // distance to enemy 2
 
-    print(closestEnemy);
+    print(dist1 < dist2 ? enemy1 : enemy2);
 }

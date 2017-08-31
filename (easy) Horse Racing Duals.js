c1305d37c53c4f8177ba7fd33d21a827eb67e39b
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2017-08-31 02:27:54 +0200

    remade - Horse Racing Duals

--- (easy) Horse Racing Duals.js
+++ (easy) Horse Racing Duals.js
@@ -1,37 +1,24 @@
 /** Horse Racing Duals (easy) https://www.codingame.com/training/easy/horse-racing-duals
  * This puzzle shows that sometimes, the simplest solution is not performant
- * enough. You will also learn to handle large arrays and gain experience with
- * their processing time.
+ * enough. You will also learn to handle large arrays and gain experience
+ * with their processing time.
  *
  * STATEMENT:
  * In this problem you have to find the two numbers that are closest to each
- * other among a list of numbers. The list might be really large and force you
- * to search for the best possible algorithmic complexity for your solution.
+ * other among a list of numbers. The list might be really large and force
+ * you to search for the best possible algorithmic complexity for your
+ * solution.
  *
  * STORY:
  * To make a race between two horses interesting, you will have to find the
  * horses with the closest strength.
 **/
-let arr = [];
-const N = parseInt(readline());
-
-for (let i = 0; i < N; i++) {
-    // read an initial array
-    arr.push(parseInt(readline()));
-}
-
-let min = 10000; // set Min
-arr.sort(function compareNumeric(a, b) {
-    // sort the initial array
-  if (a > b) return 1;
-  if (a < b) return -1;
-});
-
-for (let i = 0; i < N; i++) {
-    // look for a new Min
-    if (Math.abs(arr[i] - arr[i + 1]) < min) {
-        min = Math.abs(arr[i] - arr[i + 1]);
-    }
-}
-
-print(min);
+print(
+    new Array(+readline()) // Read Number of horses
+        .fill()              // and
+        .map(() => +readline())      // read an initial array
+        .sort((a, b) => a > b) // sort this array and
+        .map((x, i, arr) => Math.abs(x - arr[i + 1])) // calculate the differences
+        .sort((a, b)  => a > b) // sort it again
+        [0] // and print the very first element - min value
+    );

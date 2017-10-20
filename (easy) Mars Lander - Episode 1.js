19549d3efc9e120a64d993d45def013fb0c6ea58
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2017-10-20 18:09:48 +0200

    Update (easy) Mars Lander - Episode 1.js

--- (easy) Mars Lander - Episode 1.js
+++ (easy) Mars Lander - Episode 1.js
@@ -34,13 +34,13 @@ Xmiddle = X2 - (X2 - X1)/2;
 // game loop
 while (true) {
     const inputs = readline().split(' ');
-    const X = parseInt(inputs[0]);
-    const Y = parseInt(inputs[1]);
-    const HS = parseInt(inputs[2]); // the horizontal speed (in m/s), can be negative.
-    const VS = parseInt(inputs[3]); // the vertical speed (in m/s), can be negative.
-    const F = parseInt(inputs[4]); // the quantity of remaining fuel in liters.
-    const R = parseInt(inputs[5]); // the rotation angle in degrees (-90 to 90).
-    const P = parseInt(inputs[6]); // the thrust power (0 to 4).
+    const X = +(inputs[0]);
+    const Y = +(inputs[1]);
+    const HS = +(inputs[2]); // the horizontal speed (in m/s), can be negative.
+    const VS = +(inputs[3]); // the vertical speed (in m/s), can be negative.
+    const F = +(inputs[4]); // the quantity of remaining fuel in liters.
+    const R = +(inputs[5]); // the rotation angle in degrees (-90 to 90).
+    const P = +(inputs[6]); // the thrust power (0 to 4).
 
     // Write an action using print()
     // To debug: printErr('Debug messages...');

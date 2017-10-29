787bcf0939aecd1fad463156b89da8cc6d751708
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2017-10-29 11:28:20 +0100

    Update (easy) Mars Lander - Episode 1.js

--- (easy) Mars Lander - Episode 1.js
+++ (easy) Mars Lander - Episode 1.js
@@ -14,53 +14,12 @@
  * commander. You will have to land the space ship on mars, making sure that
  * the landing is done smoothly.
 **/
-const N = parseInt(readline()); // the number of points used to draw the surface of Mars.
-const X1 = -1, X2 = -1, Y1 = -1, xp = -1, Xmiddle, Ymiddle;
+const N = +readline(); // the number of points used to draw the surface of Mars.
 
 for (let i = 0; i < N; i++) {
-    const inputs = readline().split(' ');
-    const LAND_X = parseInt(inputs[0]); // X coordinate of a surface point. (0 to 6999)
-    const LAND_Y = parseInt(inputs[1]); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
-    if (LAND_Y == Y1) {
-        X1 = xp;
-        X2 = LAND_X;
-        Ymiddle = LAND_Y;
-    }
-    xp = LAND_X;
-    Y1 = LAND_Y;
+    readline();
 }
 
-Xmiddle = X2 - (X2 - X1)/2;
-// game loop
 while (true) {
-    const inputs = readline().split(' ');
-    const X = +(inputs[0]);
-    const Y = +(inputs[1]);
-    const HS = +(inputs[2]); // the horizontal speed (in m/s), can be negative.
-    const VS = +(inputs[3]); // the vertical speed (in m/s), can be negative.
-    const F = +(inputs[4]); // the quantity of remaining fuel in liters.
-    const R = +(inputs[5]); // the rotation angle in degrees (-90 to 90).
-    const P = +(inputs[6]); // the thrust power (0 to 4).
-
-    // Write an action using print()
-    // To debug: printErr('Debug messages...');
-    let angle, acc;
-    if (X < Xmiddle) {
-        angle = -45;
-        acc = (HS < 10) ? 4 :  0;
-    }
-    if (X > Xmiddle) {
-        angle = 45;
-        
-        acc = (HS > -10) ? 4 : 0;
-    }
-    if (X == Xmiddle || Y - Ymiddle < 10) {
-        angle = 0;
-    }
-    
-    if (VS < -20) {
-        angle = 0;
-        acc = 4;
-    }
-    print(angle, acc);
+    print( readline().split(' ')[3] < -39 ? '0 4' : '0 0' );
 }

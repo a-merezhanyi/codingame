73fbf07bcea9f4a862d273c2be677b41992e4603
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2017-11-04 10:06:27 +0100

    added Conway sequence

---
+++ (medium)  Conway sequence.js
@@ -0,0 +1,24 @@
+/**  Conway sequence (medium) https://www.codingame.com/training/medium/conway-sequence
+ * This puzzle validates that you can manage creation and update of lists.
+ * It also uses nested loops.
+ * 
+ * Statement:
+ * The goal of this puzzle is to make you work with a mathematical series:
+ * The Conway Sequence. Discover how to generate terms of a series
+ * interatively.
+ * 
+ * Story:
+ * A Conway Sequence is a funny mathematical object where an element
+ * describes the previous one, and recursively. Beware of cognitive exertion
+ * on this one!
+**/
+let [R, L] = [String(readline()), +readline()];
+
+for (let i=1; i < L; i++) {
+    R = R
+        .match(/(\d+)\s?(\1\s?)*/g)
+        .map( n=>(n = n.trim().split` `, `${n.length} ${n[0]}`) )
+        .join` `;
+}
+
+print(R);

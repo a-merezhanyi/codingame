bdf3cb7b60ca3f288eb72371f91140d8ccb7eff7
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2017-12-14 08:07:14 +0100

    updated "don't panic"

--- (medium) Don't Panic - Episode 1.js
+++ (medium) Don't Panic - Episode 1.js
@@ -12,32 +12,35 @@
  * allow others to reach the exit by going through the right Happy Vertical
  * People Transporters.
  **/
-let inputs = readline().split(' ')
-const nbFloors = +inputs[0] // number of floors
-const exitFloor = +inputs[3] // floor on which the exit is found
-const exitPos = +inputs[4] // position of the exit on its floor
-const nbElevators = +inputs[7] // number of elevators
-const elevators = []
+let i = readline().split` `;
+// nbFloors - number of floors
+// exitFloor - floor on which the exit is found
+// exitPos - position of the exit on its floor
+const [nbFloors, exitFloor, exitPos] = [+i[0], +i[3], +i[4]];
+let nbElevators = +i[7]; // number of elevators
+const elevators = [];
 
-for (let i = 0; i < nbElevators; i++) {
-    inputs = readline().split(' ')
-    const elevatorFloor = parseInt(inputs[0]) // floor on which this elevator is found
-    const elevatorPos = parseInt(inputs[1]) // position of the elevator on its floor
+while(nbElevators--) {
+    i = readline().split` `;
+    // elevatorFloor - floor on which this elevator is found
+    // elevatorPos - position of the elevator on its floor
+    const [elevatorFloor, elevatorPos] = [+i[0], +i[1]];
     
-    elevators[parseInt(inputs[0])] = parseInt(inputs[1]) // portals' positions into an array 
+    elevators[+i[0]] = +i[1]; // portals' positions into an array 
 }
 // game loop
 while (true) {
-    inputs = readline().split(' ')
-    const cloneFloor = parseInt(inputs[0]) // floor of the leading clone
-    const clonePos = parseInt(inputs[1]) // position of the leading clone on its floor
-    const direction = inputs[2] // direction of the leading clone: LEFT or RIGHT
-    const currentExitPos = (cloneFloor === exitFloor) ? exitPos : elevators[cloneFloor];
+    i = readline().split` `;
+    // cloneFloor - floor of the leading clone
+    // clonePos - position of the leading clone on its floor
+    // direction - direction of the leading clone: LEFT or RIGHT
+    const [cloneFloor, clonePos, direction] = [+i[0], +i[1], i[2]]; 
+    const currentExitPos = cloneFloor === exitFloor ? exitPos : elevators[cloneFloor];
     const goingFalseDirection = ( // detect false direction
         clonePos > currentExitPos && direction === 'RIGHT' ||
         clonePos < currentExitPos && direction === 'LEFT')
         ? true
-        : false
+        : false;
     
-    print(goingFalseDirection ? 'BLOCK' : 'WAIT')
+    print(goingFalseDirection ? 'BLOCK' : 'WAIT');
 }

f9cf7a196c87d42ac1af829a67630bda2a32e432
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2017-12-15 09:27:26 +0100

    updated - no Spoon

--- (medium) There is no Spoon - Episode 1.js
+++ (medium) There is no Spoon - Episode 1.js
@@ -16,35 +16,34 @@
  * order to give humanity a decisive tactical advantage. First phase: code the
  * initialization mechanism of the APU.
  **/
-const width = parseInt(readline()); // the number of cells on the X axis
-const height = parseInt(readline()); // the number of cells on the Y axis
-let matrix = [];
+// width - the number of cells on the X axis
+// height - the number of cells on the Y axis
+const [width, height] = [+readline(), +readline()]; 
 
-for (let i = 0; i < height; i++) { // store all rows
-    matrix.push(readline()); // width characters, each either 0 or .
-}
+// store all rows
+// width characters, each either 0 or .
+const matrix = [...Array(height)].map(readline);
 
 for (let y1 = 0; y1 < height; y1++) {
     for (let x1 = 0; x1 < width; x1++) {
-        if (matrix[y1].charAt(x1) === '.') {
+        if ('.' === matrix[y1].charAt(x1)) {
             continue; // stop iterating if this cell is empty
         }
-
-        let x2 = y2 = '-1';
-        let x3 = y3 = '-1';
-
-        const nextX = +(matrix[y1].slice(x1 + 1).indexOf('0') + 1); // get the very right Cell X2
-        x2 = (nextX && x1 + 1 < width) ? nextX + x1 : '-1';
-
+        
+        let [x2, y2, x3, y3] = ['-1', '-1', '-1', '-1'];
+        
+        const nextX = +(matrix[y1].slice(x1+1).indexOf('0') + 1); // get the very right Cell X2
+        x2 = (nextX && x1+1 < width) ? nextX + x1 : '-1';
+        
         for (let j = y1 + 1; j < height; j++) {
             if ('0' === matrix[j].charAt(x1)) { // get the very next below Cell Y3
                 y3 = j; // store current position and
                 break; // stop irerating if found
             }
         }
-
-        y2 = (x2 !== '-1') ? y1 : '-1'; // add to the result Right Cell X2 Y2
-        x3 = (y3 !== '-1') ? x1 : '-1'; // add to the result Below Cell X3 Y3
+        
+        y2 = ('-1' !== x2) ? y1 : '-1'; // add to the result Right Cell X2 Y2
+        x3 = ('-1' !== y3) ? x1 : '-1'; // add to the result Below Cell X3 Y3
         // Three coordinates: a node, its right neighbor, its bottom neighbor
         print(`${x1} ${y1} ${x2} ${y2} ${x3} ${y3}`); // print cells
     }

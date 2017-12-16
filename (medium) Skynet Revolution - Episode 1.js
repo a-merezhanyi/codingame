c630725fde275d26cc790c54bc794e3084986f4c
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2017-12-17 00:16:50 +0100

    updated Skynet

--- (medium) Skynet Revolution - Episode 1.js
+++ (medium) Skynet Revolution - Episode 1.js
@@ -13,38 +13,38 @@
  * But Skynet is sending its agents to fight back! Prevent Skynet from
  * destroying the virus by severing the right network links...
  */
-const inputs = readline().split` `;
-const [N, L, E] = [+inputs[0], +inputs[1], +inputs[2]];
-// N - the total number of nodes in the level, including the gateways
+// N - the total number of nodes in the level, including the gates
 // L - the number of links
-// E - the number of exit gateways
-
+// E - the number of exit gates
+let [N, L, E] = readline().split` `.map(Number);
 const nodes = new Array(N).fill().map(_ => []);
 
-for (let i = 0; i < L; i++) {
-    const inputs = readline().split` `;
-    const [N1, N2] = [+inputs[0], +inputs[1]];
+while (L--) {
+    const [N1, N2] = readline().split` `.map(Number);
     // N1 and N2 defines a link between these nodes
-    // create a hashmap
+    // create a Graph
     nodes[N1][N2] = N2;
     nodes[N2][N1] = N1;
 }
 
-// the index of a gateway node
+// the index of a gate node
 const EI = new Array(E).fill().map(_ => +readline());
 
 // game loop
-while (true) {
+while (1) {
     const SI = +readline();
     // SI is the index of the node on which the Skynet agent is positioned this turn
     for (let i = 0; i < E; i++) {
-        if (nodes[SI][EI[i]]) {
-             print(`${SI} ${EI[i]}`);
-             delete nodes[SI][EI[i]];
+        const node = nodes[SI]; 
+        const gate = EI[i];
+        
+        if (node[gate]) {
+            print(`${SI} ${gate}`);
+            delete node[gate];
             break;
-        } else if (i === E - 1) {
-             print(`${SI} ${nodes[SI][Object.keys(nodes[SI])[0]]}`);
-            delete nodes[SI][Object.keys(nodes[SI])[0]];
+        } else if (E - 1 === i) {
+            print(`${SI} ${node[Object.keys(node)[0]]}`);
+            delete node[Object.keys(node)[0]];
         }
     }
 }

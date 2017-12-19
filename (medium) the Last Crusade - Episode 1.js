e35c24007acd029b4352c9b9ec2dc24af16b0aa1
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2017-12-19 10:04:48 +0100

    updated Indy

--- (medium) the Last Crusade - Episode 1.js
+++ (medium) the Last Crusade - Episode 1.js
@@ -1,45 +1,42 @@
 /** the Last Crusade - Ep. 1 (medium) https://www.codingame.com/training/medium/the-last-crusade-episode-1
  * This puzzle makes you use an associative array that make a link between
- * arbitrary types and directions. You also need to store some values in a 2D array.
+ * arbitrary types and directions. You also need to store some values in a
+ * 2D array.
  * 
  * Statement:
- * The goal of this puzzle is to predict the path that a character will take in
- * a labyrinth according to the topology of the rooms. The resolution of this
- * exercise intensively focus on the correct usage of associative arrays. If
- * you can manage them correctly and creates the right associations, your final
- * code could be quite short.
+ * The goal of this puzzle is to predict the path that a character will take
+ * in a labyrinth according to the topology of the rooms. The resolution
+ * of this exercise intensively focus on the correct usage of associative
+ * arrays. If you can manage them correctly and creates the right
+ * associations, your final code could be quite short.
  * 
  * Story:
  * Indiana is trapped in a tunnel, help him escape!
  * In this first level, you just have to get familiar with how the tunnel
- * works: your goal is simply to predict Indiana movements within this tunnel.
+ * works: your goal is simply to predict Indiana movements within this
+ * tunnel.
  **/
-let inputs = readline().split(' ');
-const W = parseInt(inputs[0]); // Number of columns.
-const H = parseInt(inputs[1]); // Number of rows.
+// W - Number of columns.
+// H - Number of rows.
+let [W, H] = readline().split` `.map(Number); 
 const maze = []; // Maze
 
-for (let i = 0; i < H; i++) {
+while (H--) {
     // Represents a line in the grid and contains W integers.
     // Each integer represents one room of a given type.
-    const LINE = readline();
-    maze.push(LINE);
-
+    maze.push(readline());
 }
 // The coordinate along the X axis of the exit
 // (not useful for this first mission, but must be read).
-const EX = parseInt(readline());
+const EX = +readline();
 
 // game loop
 while (true) {
-    inputs = readline().split(' ');
-    const XI = parseInt(inputs[0]);
-    const YI = parseInt(inputs[1]);
-    const POS = inputs[2];
-
-    const currentLevel = maze[YI].split(' ');
-    let XT = XI;
-    let YT = YI;
+    inputs = readline().split` `;
+    const [XI, YI, POS] = [+inputs[0], +inputs[1], inputs[2]];
+    const currentLevel = maze[YI].split` `;
+    let [XT, YT] = [XI, YI];
+    
     switch(currentLevel[XI]) {
         case '1':
         case '3':
@@ -64,7 +61,7 @@ while (true) {
             break;
         case '10':
             XT -= 1;
-            break;    
+            break;
         case '11':
             XT += 1;
             break;
